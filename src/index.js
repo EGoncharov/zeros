fd1c58f9721b2296a805459f081d4e6e4f6ca0c2
Eugene Goncharov <eugen.goncharov@gmail.com>
2018-09-30 23:25:33 +0300

    Task done 12s

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 module.exports = function getZerosCount(number) {
-  // your implementation
+ var zerosCount = 0;
+ for (var i = 5; i <= number; i += 5) {
+ 	var y = i;
+ 	while( y % 5 === 0) { y = y / 5; zerosCount++;}
+ 	}
+ 	return zerosCount;
 }
